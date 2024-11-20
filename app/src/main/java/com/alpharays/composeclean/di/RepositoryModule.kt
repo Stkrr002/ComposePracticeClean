96a2d74a1207adcd4f53569eeb296ffd3667b18b
stkrr002 <sumitkrr2002@gmail.com>
2024-11-20 13:28:59 +0530

    repository done

--- app/src/main/java/com/alpharays/composeclean/di/RepositoryModule.kt
+++ app/src/main/java/com/alpharays/composeclean/di/RepositoryModule.kt
@@ -1,4 +1,17 @@
 package com.alpharays.composeclean.di
 
-class RepositoryModule {
+import com.alpharays.composeclean.data.UserRepositoryImpl
+import com.alpharays.composeclean.domain.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun providesUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
 }
