1d1696422e9de6fce40f4d5b8eab47e8f2de1fe8
stkrr002 <sumitkrr2002@gmail.com>
2024-11-20 13:00:15 +0530

    mid commit

--- app/src/main/java/com/alpharays/composeclean/data/UserRepositoryImpl.kt
+++ app/src/main/java/com/alpharays/composeclean/data/UserRepositoryImpl.kt
@@ -1,11 +1,19 @@
 package com.alpharays.composeclean.data
 
 import com.alpharays.composeclean.domain.UserRepository
+import com.alpharays.composeclean.utils.APIResponse
+import com.alpharays.composeclean.utils.ResponseHandler
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(private val apiServices: ApiServices) :
     UserRepository {
-    override suspend fun getUserList(): List<String> {
-        return apiServices.getUserList()
-    }
+
+    @Inject
+    lateinit var responseHandler: ResponseHandler
+    override suspend fun getUserList(): Flow<APIResponse<List<String>>> =
+
+        responseHandler.callAPI {
+            apiServices.getUserList("")
+        }
 }
--- app/src/main/java/com/alpharays/composeclean/domain/UserRepository.kt
+++ app/src/main/java/com/alpharays/composeclean/domain/UserRepository.kt
@@ -1,6 +1,9 @@
 package com.alpharays.composeclean.domain
 
+import com.alpharays.composeclean.utils.APIResponse
+import kotlinx.coroutines.flow.Flow
+
 interface UserRepository {
-    suspend fun getUserList(): List<String>
+    suspend fun getUserList(): Flow<APIResponse<List<String>>>
 
 }
