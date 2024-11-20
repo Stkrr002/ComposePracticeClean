b8fa2fec4c4c20c4620960d2a648c3b42d164af0
stkrr002 <sumitkrr2002@gmail.com>
2024-11-20 13:36:04 +0530

    repository done

--- app/src/main/java/com/alpharays/composeclean/di/LocalAppModule.kt
+++ app/src/main/java/com/alpharays/composeclean/di/LocalAppModule.kt
@@ -1,4 +1,36 @@
 package com.alpharays.composeclean.di
 
+import com.alpharays.composeclean.utils.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class LocalAppModule {
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit.Builder {
+        return Retrofit.Builder().baseUrl("baseUrl")
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        interceptor: AuthInterceptor
+    ): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val builder =
+            OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(loggingInterceptor)
+        return builder.build()
+    }
 }
